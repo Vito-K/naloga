515da9417dd2e8daa323cc01028b6ab1b6c2d591
Vito-K <vitokuki@gmail.com>
2021-03-26 17:13:14 +0100

    Update3

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\regController;
+use App\Http\Controllers\dashController;
+use App\Http\Controllers\Auth\loginController;
+use App\Http\Controllers\Auth\logoutController;
+use App\Http\Controllers\postController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+Route::get('/dashboard', [dashController::class, 'index'])
+    ->name('dashboard');
+
+Route::post('/logout', [logoutController::class, 'store'])->name('logout');
+
+Route::get('/register', [regController::class, 'index'])->name('register');
+Route::post('/register', [regController::class, 'store']);
+
+Route::get('/login', [loginController::class, 'index'])->name('login');
+Route::post('/login', [loginController::class, 'store']);
+
+Route::get('/posts', [postController::class, 'index'])->name('posts');
+Route::post('/posts', [postController::class, 'store']);
