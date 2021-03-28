30a8e3e5f21cfc19fb6a13847e27598bb6951616
Vito-K <vitokuki@gmail.com>
2021-03-28 18:30:05 +0200

    Update4

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class postController extends Controller
 {
     public function index()
     {
+        $posts = Post::get();
         return view('posts.index');
     }
 
     public function store(Request $request)
     {
-        
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
     }
 }
